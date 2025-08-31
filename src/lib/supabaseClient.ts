a498dcc5e6bd4e0ad895a3f8737126b33409dd53
venkatakrishna21 <70277422+venkatakrishna21@users.noreply.github.com>
2025-08-31 18:05:14 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
+"use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import type { Database } from "../types/database"; // relative path
 
-export const supabaseBrowser = createClientComponentClient<Database>();
+export const supabaseBrowser = createClientComponentClient();
