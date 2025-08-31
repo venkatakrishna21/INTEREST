c5f8746d6ce195b3bc2d7fc07864a31ccde8516e
venkatakrishna21 <70277422+venkatakrishna21@users.noreply.github.com>
2025-08-31 19:00:50 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,13 +1,5 @@
+'use client';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseBrowser = () => {
-  return createClientComponentClient();
-};
-
-// Fallback for server / cron jobs
-export const supabaseServer = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-);
+export const supabaseBrowser: SupabaseClient = createClientComponentClient();
